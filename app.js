ade6cae2b06e14b2d0f8ab6f516e513c95372a28
Askaholic <askaholic907@gmail.com>
2018-04-18 14:55:07 -0800

    Added mongoose

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+var bluebird = require('bluebird');
 var express = require('express');
+var mongoose = require('mongoose');
 
+const SERVER_URL = 'http://localhost:4200'
 
 var app = express();
+
+mongoose.Promise = bluebird;
+mongoose.connect('mongodb://127.0.0.1:27017/dopebeets')
+    .then(() => {
+        console.log('Successfully connected to database');
+    })
+    .catch(() => {
+        console.log('Could not connect to database!');
+    })
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", SERVER_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
